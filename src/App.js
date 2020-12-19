a7cedaa18d4f8be0a2c23055d21b81b5d4427a40
secimp <secimpalut28@gmail.com>
2020-12-19 20:08:38 +0200

    Adding crousel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router ,Switch , Route} from "react-router-dom";
 import Header from './header/Header';
+import Home from './home/Home';
 
 function App() {
   return <Router>
@@ -10,7 +11,7 @@ function App() {
     <Switch>
       <Route path="/login">Login</Route>
       <Route path="/register">Register</Route>
-      <Route path=""></Route>
+      <Route path="/" component={Home} />
     </Switch>
     </div>
   </Router>;
---
+++ src/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./home.css";
+import "./Banner";
+import Banner from './Banner';
+function Home() {
+    return (
+        <div className="home">
+            <Banner />
+        </div>
+    )
+}
+export default Home
