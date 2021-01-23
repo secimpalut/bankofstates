261d7924da9d6c37764ceced8618fb29e025da9f
secimp <secimpalut28@gmail.com>
2021-01-23 19:54:55 +0200

    23 jan

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router ,Switch , Route} from "react-router-dom";
-import Header from './header/Header';
-import Home from './home/Home';
-import Footer from './footer/Footer';
-
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Home from "./home/Home";
+import Login from "./login/Login";
+import Register from "./register/Register";
+import Admin from "./admin/Admin";
+import User from "./user/User";
 function App() {
-  return <Router>
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/login">Login</Route>
-        <Route path="/register">Register</Route>
-        <Route path="/" component={Home} />
-      </Switch>
-      <Footer />
-    </div>
-  </Router>;
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+              <Register />
+          </Route>
+          <Route path="/admin">
+              <Admin />
+          </Route>
+          <Route path="/user">
+              <User />
+          </Route>
+          <Route path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
-
 export default App;
